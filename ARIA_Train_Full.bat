4b526cf98cff85adbd866c02d518cb8da40b2436
lold3344 <kulikovt537@gmail.com>
2026-06-16 00:52:42 +1000

    git ignore

--- ARIA_Train_Full.bat
+++ ARIA_Train_Full.bat
@@ -2,11 +2,12 @@
 chcp 65001 >nul
 title ARIA Full Training - 14.6M sequences / 1M vocab
 cd /D "C:\Users\lold\Documents\GitHub\ARIA"
+set PATH=%USERPROFILE%\.cargo\bin;%PATH%
 set ARIA_MAX_SEQS=14600000
 set ARIA_VOCAB_LINES=1000000
 echo =========================================
 echo   ARIA Full Training
-echo   MAX_SEQS  = %ARIA_MAX_SEQS%
+echo   MAX_SEQS  = %ARIA_MAX_SEQS%  
 echo   VOCAB     = %ARIA_VOCAB_LINES%
 echo   Working dir: %CD%
 echo =========================================
